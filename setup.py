171ad73f07227ed6ff8551254f284c45d48b99bb
Florian Henkel <florian.henkel@jku.at>
2021-05-05 15:28:39 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+import setuptools
+
+from setuptools import setup
+
+
+setup(
+    name='contextual_gridworld',
+    version='0.1dev',
+    description='Environment for Contextual Reinforcement Learning',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: ReinforcementLearning",
+    ],
+    author='Hamid Eghbal-zadeh, Florian Henkel',
+)
